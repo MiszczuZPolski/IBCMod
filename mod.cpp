78715283d6a10c40704ab76a9bd3d755348a4e2f
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2022-11-21 09:39:08 +0100

    project templ update

--- mod.cpp
+++ mod.cpp
@@ -1,12 +1,12 @@
 name = "IBC Main Mod";
-picture = "logo_proj_templ_ca.paa";
+picture = "logo_ibc_ca.paa";
 actionName = "GitHub";
 action = "https://github.com/MiszczuZPolski/IBCMod";
 description = "Mod for Arma 3";
-logo = "logo_proj_templ_ca.paa";
-logoOver = "logo_proj_templ_ca.paa";
+logo = "logo_ibc_ca.paa";
+logoOver = "logo_ibc_ca.paa";
 tooltip = "ACE3 Project Template";
 tooltipOwned = "ACE3 Project Template Owned";
 overview = "Advanced Combat Environment 3 Project Template";
 author = "MiszczuZPolski";
-overviewPicture = "logo_ace3_ca.paa";
+overviewPicture = "logo_ibc_ca.paa";
