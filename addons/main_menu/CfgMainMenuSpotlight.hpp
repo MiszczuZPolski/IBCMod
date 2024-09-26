af8f885209a436b2bfc430754136dc94cb6d3e27
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2024-09-26 21:00:46 +0200

    Update CfgMainMenuSpotlight.hpp

--- addons/main_menu/CfgMainMenuSpotlight.hpp
+++ addons/main_menu/CfgMainMenuSpotlight.hpp
@@ -37,22 +37,27 @@ class CfgMainMenuSpotlight {
     delete AoW_Showcase_AoW;
     delete AoW_Showcase_Future;
 
-    // Western Sahara
     delete Extraction_lxWS;
     delete Showcase_Alchemist_lxWS;
     delete Showcase_VR_lxWS;
 
-    // S.O.G. Prairie Fire
     delete vn_showcase_macv;
     delete vn_showcase_macv_13;
     delete vn_showcase_pavn;
     delete vn_showcase_pavn_13;
     delete vn_sogba;
 
-    // Spearhead 1944
     delete SPE_Arsenal;
     delete SPE_Campagin_OperationCobra_04;
-    delete SPE_Coop_DerZahnarzt;
+    delete SPE_Coop_Der_Zahnarzt;
     delete SPE_Faction_Showcase_GER;
     delete SPE_Faction_Showcase_US;
+    delete SPE_Coop_Attack_on_Mortain;
+    delete SPE_Coop_Battle_of_St_Barthelemy;
+    delete SPE_Coop_Panzerkampfwagen;
+    delete SPE_Coop_Prizefighter;
+
+    delete Scenario_TrainingDay_RF;
+    delete Scenario_FireSeason_RF;
+    delete Scenario_AirControl_RF;
 };
