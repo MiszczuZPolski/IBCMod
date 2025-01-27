090b1616e6204da473a0455576286181194b1084
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2025-01-27 21:35:44 +0100

    example

--- addons/compat_csa38/CfgWeapons.hpp
+++ addons/compat_csa38/CfgWeapons.hpp
@@ -1,3 +1,11 @@
 class CfgWeapons {
+    class Default {};
+	class RifleCore: Default {};
+    class Rifle: RifleCore {};
+	class Rifle_Base_F: Rifle {};
+	class CSA38_vz24: Rifle_Base_F {};
+	class CSA38_kar98k: CSA38_vz24 {
+        displayName = CSTRING(kar98k);
+    };
 
 };
