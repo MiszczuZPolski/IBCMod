f21369e229f32146bb6ddb12fe007675b6d7fe3f
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2025-01-31 18:33:51 +0100

    Machineguns done

--- addons/compat_csa38/CfgWeapons.hpp
+++ addons/compat_csa38/CfgWeapons.hpp
@@ -1,11 +1,55 @@
 class CfgWeapons {
-    class Default {};
-	class RifleCore: Default {};
-    class Rifle: RifleCore {};
+	class Default {};
+    //Rifles
+    class RifleCore: Default {};
+	class Rifle: RifleCore {};
 	class Rifle_Base_F: Rifle {};
-	class CSA38_vz24: Rifle_Base_F {};
-	class CSA38_kar98k: CSA38_vz24 {
-        displayName = CSTRING(kar98k);
+	class Rifle_Long_Base_F: Rifle_Base_F {};
+	class CSA38_MGBASE: Rifle_Long_Base_F {};
+    class CSA38_BRENMKII: CSA38_MGBASE {
+        opticsZoomMax = 1.25;
+        opticsZoomMin = 0.25;
+    };
+    class CSA38_BRENMKII2: CSA38_BRENMKII {
+        opticsZoomMax = 1.25;
+        opticsZoomMin = 0.25;
+    };
+	class CSA38_BRENMKI: CSA38_BRENMKII {
+        opticsZoomMax = 1.25;
+        opticsZoomMin = 0.25;
+    };
+    class CSA38_BRENMKI2: CSA38_BRENMKII {
+        opticsZoomMax = 1.25;
+        opticsZoomMin = 0.25;
+    };
+    class CSA38_BRENMKIII: CSA38_BRENMKII {
+        opticsZoomMax = 1.25;
+        opticsZoomMin = 0.25;
+    };
+    class CSA38_BRENMKIII2: CSA38_BRENMKIII {
+        opticsZoomMax = 1.25;
+        opticsZoomMin = 0.25;
+    };
+    class CSA38_MG30: CSA38_MGBASE {
+        opticsZoomMax = 1.25;
+        opticsZoomMin = 0.25;
+    };
+	class CSA38_MG34: CSA38_MGBASE {
+        opticsZoomMax = 1.25;
+        opticsZoomMin = 0.25;
+    };
+    class CSA38_zb26: CSA38_MGBASE {
+        opticsZoomMax = 1.25;
+        opticsZoomMin = 0.25;
     };
 
+    //Static weapons
+    class LauncherCore: Default {};
+	class Launcher: LauncherCore {};
+	class Launcher_Base_F: Launcher {};
+	class csa38_m36hlaven: Launcher_Base_F {};
+	class csa38_brentripod2: csa38_m36hlaven {
+        opticsZoomMax = 1.25;
+        opticsZoomMin = 0.25;
+    };
 };
