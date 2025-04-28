03cb17f8ffdc0d9269a0681beb44e7a56e61f2ec
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2025-04-28 20:32:01 +0200

    remove cba settings

--- addons/cba_settings_userconfig/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define COMPONENT cba_settings_userconfig
-#define COMPONENT_BEAUTIFIED IBC CBA Settings
-
-#include "\z\ibc\addons\main\script_mod.hpp"
-#include "\z\ibc\addons\main\script_macros.hpp"
