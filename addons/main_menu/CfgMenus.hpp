059c17bfe37ab6f95dfed5ee8ecc31e2a5d5616c
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2025-09-18 18:39:42 +0200

    fix classes

--- addons/main_menu/CfgMenus.hpp
+++ addons/main_menu/CfgMenus.hpp
@@ -1,11 +1,11 @@
 class EMM_mainMenu_CfgMenus {
     class VN {
         class menus {
-            class MainMenu;
+            class MainMenu {};
             class MultiplayerVN: MainMenu {
                 items[] = {"IBC", "ServerBrowser", "SOGPrairieFire", "MikeForce", "Exit"};
 
-                class ServerBrowser;
+                class ServerBrowser {};
                 class IBC: ServerBrowser {
                     action = QUOTE(_this call (uiNamespace getVariable QQFUNC(join)));
                     text = CSTRING(SpotlightHeader);
